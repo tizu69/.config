588a6508b3b2441116f52d19a5bb89c142821791
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-07-29 09:40:35 +0200

    renovate startups

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -1,2 +1,10 @@
+# mumble redirection loop for phone-to-computer microphone using mumble bridge
 pactl load-module module-null-sink sink_name=mumble sink_properties=device.description="Mumble"
 pactl load-module module-remap-source source_name=mumbleuser source_properties=device.description="MumbleUser" master=mumble.monitor
+
+# autostart!
+/usr/lib/polkit-kde-authentication-agent-1 &                            # polkit
+waybar & hyprpaper & dunst & hyprpaper &                                # system level apps
+vesktop & materialgram & google-chrome-unstable &                       # essential apps
+steam -silent & kdeconnectd & ckb-next -b &                             # background startup apps
+disown -a
