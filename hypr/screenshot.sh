b696fa373b144169154d41e77a619f2a154933ef
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-10-12 08:56:06 +0200

    squweenshots

--- hypr/screenshot.sh
+++ hypr/screenshot.sh
@@ -1,15 +1,2 @@
-grim -c -g "$(slurp)" - | magick png:- -shave 1x1 png:- | wl-copy -t image/png
+grimblast --freeze copy area
 notify-send "Screenshot copied to clipboard"
-
-# | magick png:- -shave 1x1 \
-# png:- | magick png:- \
-# \( +clone  -alpha extract \
-#   -draw 'fill black polygon 0,0 0,15 15,0 fill white circle 15,15 15,0' \
-#   \( +clone -flip \) -compose Multiply -composite \
-#   \( +clone -flop \) -compose Multiply -composite \
-# \) -alpha off -compose CopyOpacity -composite \
-# png:- | magick png:- \
-# -bordercolor none -border 10x10 \
-# \( +clone -background black -shadow 50x5+0+5 \) \
-# +swap -background none -layers merge +repage \
-# png:- \
